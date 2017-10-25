92a79add1eda7c364def4187d9af51b6f22c1f91
Nathan Wright <nathan@brandalism.com>
2017-10-25 01:23:49 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+
+const parts = require('./webpack.parts');
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  dist: path.join(__dirname, 'dist'),
+};
+
+const commonConfig = merge([
+  {
+    entry: {
+      index: PATHS.src,
+    },
+    output: {
+      path: PATHS.dist,
+      filename: '[name].js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack demo',
+      }),
+    ],
+  },
+  parts.lintJavaScript({ include: PATHS.src }),
+]);
+
+const productionConfig = merge([
+  parts.extractCSS({
+    use: ['css-loader', parts.autoprefix()],
+  }),
+]);
+
+const developmentConfig = merge([
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+  parts.loadCSS(),
+]);
+
+module.exports = (env) => {
+  if (env === 'production') {
+    return merge(commonConfig, productionConfig);
+  }
+
+  return merge(commonConfig, developmentConfig);
+};
---
+++ webpack.parts.js
@@ -0,0 +1,80 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+exports.devServer = ({ host, port } = {}) => ({
+  devServer: {
+    historyApiFallback: true,
+    stats: 'errors-only',
+    host, // Defaults to `localhost`
+    port, // Defaults to 8080
+    overlay: {
+      errors: true,
+    //   // warnings: true,
+    },
+  },
+});
+
+exports.lintJavaScript = ({ include, exclude, options }) => ({
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include,
+        exclude,
+        enforce: 'pre',
+
+        loader: 'eslint-loader',
+        options,
+      },
+    ],
+  },
+});
+
+exports.loadCSS = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        include,
+        exclude,
+
+        use: ['style-loader', 'css-loader', 'fast-sass-loader', autoprefix()],
+      },
+    ],
+  },
+});
+
+exports.extractCSS = ({ include, exclude, use }) => {
+  // Output extracted CSS to a file
+  const plugin = new ExtractTextPlugin({
+    filename: '[name].css',
+  });
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          include,
+          exclude,
+
+          use: plugin.extract({
+            use,
+            fallback: ['style-loader'],
+          }),
+        },
+      ],
+    },
+    plugins: [ plugin ],
+  };
+};
+
+const autoprefix = () => ({
+  loader: 'postcss-loader',
+  options: {
+    plugins: () => ([
+      require('autoprefixer')(),
+    ]),
+  },
+});
+
+exports.autoprefix = autoprefix;
